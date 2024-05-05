1e6db199d4312aab970522672f7932ec1492a139
Louis Ramos <louisramosdev@gmail.com>
2024-05-05 18:13:41 +0200

    feat(sub categories): create SubCategoriesController#create

--- api/app/controllers/sub_categories_controller.rb
+++ api/app/controllers/sub_categories_controller.rb
@@ -9,9 +9,23 @@ class SubCategoriesController < ApplicationController
     render json: @sub_category, status: :ok
   end
 
+  def create
+    @sub_category = current_user.sub_categories.new(sub_category_params)
+
+    if @sub_category.save
+      render json: @sub_category, status: :created
+    else
+      render json: { errors: @sub_category.error_codes }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_sub_category
     @sub_category = SubCategory.find(params[:id])
+  end
+
+  def sub_category_params
+    params.require(:sub_category).permit(:category_id, :name)
   end
 end
--- api/spec/controller/sub_categories_controller_spec.rb
+++ api/spec/controller/sub_categories_controller_spec.rb
@@ -69,4 +69,75 @@ RSpec.describe SubCategoriesController, type: :request do
       end
     end
   end
+
+  describe "POST /sub_categories" do
+    let(:category) { create(:category) }
+    let(:name) { "My sub category" }
+
+    context "when a User is authenticated" do
+      before do
+        sign_in user
+      end
+
+      context "with proper params" do
+        before do
+          post "/sub_categories", params: { sub_category: { category_id: category.id, name: } }
+        end
+
+        it "returns a JSON object" do
+          expect(response.body).to be_a String
+          expect(response.parsed_body).to have_key "id"
+        end
+
+        it "creates an instance of SubCategory" do
+          expect(SubCategory.count).to eq 1
+        end
+
+        it "returns the new instance of SubCategory" do
+          data = response.parsed_body
+          expect(data).to include({
+            "name" => name
+          })
+        end
+
+        it "returns a created HTTP status" do
+          expect(response).to have_http_status :created
+        end
+      end
+
+      context "without proper params" do
+        before do
+          post "/sub_categories", params: { sub_category: { category_id: nil, name: nil } }
+        end
+
+        it "returns a JSON object" do
+          expect(response.body).to be_a String
+          expect(response.parsed_body).to have_key "errors"
+        end
+
+        it "does not create an instance of SubCategory" do
+          expect(SubCategory.count).to eq 0
+        end
+
+        it "returns a list of error messages" do
+          data = response.parsed_body
+          expect(data["errors"]).to eq({
+            "category" => %w[blank],
+            "name" => %w[blank]
+          })
+        end
+
+        it "returns a unprocessable_entity HTTP status" do
+          expect(response).to have_http_status :unprocessable_entity
+        end
+      end
+    end
+
+    context "when a User is not authenticated" do
+      it "returns a unauthorized HTTP status" do
+        post "/sub_categories", params: { sub_category: { category_id: category.id, name: } }
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+  end
 end
