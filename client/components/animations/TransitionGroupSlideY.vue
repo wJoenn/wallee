d8f1f36da52d537aa75feea66b0bef65231e3ea8
Louis Ramos <louisramosdev@gmail.com>
2024-05-06 20:32:35 +0200

    feat(animations): create TransitionGroupSlideY

---
+++ client/components/animations/TransitionGroupSlideY.vue
@@ -0,0 +1,37 @@
+<template>
+  <TransitionGroup name="slide-y" tag="div">
+    <slot />
+  </TransitionGroup>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    from: number
+  }>()
+
+  defineSlots<{
+    default: never
+  }>()
+
+  const css = computed(() => ({
+    transform: `translateY(calc(0.25rem * ${props.from}))`
+  }))
+</script>
+
+<style>
+  .slide-y-enter-from,
+  .slide-y-leave-to {
+    opacity: 0;
+    transform: v-bind("css.transform");
+  }
+
+  .slide-y-enter-active,
+  .slide-y-leave-active,
+  .slide-y-move {
+    transition: all 0.3s ease;
+  }
+
+  .slide-y-leave-active {
+    position: absolute;
+  }
+</style>
