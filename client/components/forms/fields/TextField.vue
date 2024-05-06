aca5e920a92eeb95b0294450384a79ef4a42d862
Louis Ramos <louisramosdev@gmail.com>
2024-05-06 20:36:51 +0200

    feat(forms): create TextField

---
+++ client/components/forms/fields/TextField.vue
@@ -0,0 +1,25 @@
+<template>
+  <BaseField :errors :label :name>
+    <BaseInput
+      :id="name"
+      v-model:value="value"
+      :disabled
+      :is-invalid="errors.length > 0"
+      :name
+      :placeholder
+      type="text"
+    />
+  </BaseField>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    disabled?: boolean
+    label: string
+    name: string
+    placeholder?: string
+  }>()
+
+  // eslint-disable-next-line vue/no-setup-props-reactivity-loss
+  const { errors, value } = useField<string>(props.name)
+</script>
