a9e7a20d3272b11767eeffe3bb6064ac811717d5
Louis Ramos <louisramosdev@gmail.com>
2024-05-08 22:34:04 +0200

    feat(ui): create BaseButton

---
+++ client/components/ui/BaseButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="base-button" :type>
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+  const props = withDefaults(defineProps<{
+    mode?: "primary" | "secondary"
+    type?: "button" | "submit"
+  }>(), {
+    mode: "primary",
+    type: "button"
+  })
+
+  defineSlots<{
+    default: never
+  }>()
+
+  const css = computed(() => ({
+    backgroundColor: `var(--color-${props.mode})`,
+    color: `var(${props.mode === "primary" ? "--background-primary" : "--text-primary"})`
+  }))
+</script>
+
+<style scoped>
+  .base-button {
+    background-color: v-bind("css.backgroundColor");
+    border-radius: 0.25rem;
+    color: v-bind("css.color");
+    padding: 0.75rem 1rem;
+  }
+</style>
