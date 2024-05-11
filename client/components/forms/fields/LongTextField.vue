b6e78528072238ff0e4c1adaae69d6808272959a
Louis Ramos <louisramosdev@gmail.com>
2024-05-11 13:01:45 +0200

    feat(forms): create LongTextField

---
+++ client/components/forms/fields/LongTextField.vue
@@ -0,0 +1,47 @@
+<template>
+  <BaseField :errors :label :name>
+    <textarea
+      v-model="value"
+      :id="name"
+      class="long-text-field"
+      :class="{ invalid: errors.length > 0 }"
+      :name :placeholder
+    />
+  </BaseField>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    disabled?: boolean
+    label: string
+    name: string
+    placeholder?: string
+  }>()
+
+  // eslint-disable-next-line vue/no-setup-props-reactivity-loss
+  const { errors, value } = useField<string>(props.name)
+</script>
+
+<style scoped>
+  .long-text-field {
+    background-color: var(--background-primary);
+    border: 1px solid var(--color-secondary);
+    border-radius: 0.25rem;
+    padding: 0.75rem 1rem;
+    transition: all 0.3s ease;
+
+    &:focus {
+      border: 1px solid var(--color-primary);
+      outline: none;
+    }
+
+    &.invalid {
+      background-color: var(--background-negative);
+      border: 1px solid var(--text-negative);
+
+      &:focus {
+        border: 1px solid var(--text-negative);
+      }
+    }
+  }
+</style>
