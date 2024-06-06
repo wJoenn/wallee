4b059741e9b240fa2d2ae3044bb0c7522878f229
Louis Ramos <louisramosdev@gmail.com>
2024-06-06 14:55:32 +0200

    feat(ui): create BaseModal

---
+++ client/components/ui/BaseModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <IonModal :is-open :presenting-element>
+    <div class="base-modal">
+      <slot />
+    </div>
+  </IonModal>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    isOpen: boolean
+    presentingElement?: HTMLElement
+  }>()
+
+  defineSlots<{
+    default: never
+  }>()
+</script>
+
+<style scoped>
+  .base-modal {
+    background: var(--background-primary);
+    color: var(--text-primary);
+    height: 100%;
+    padding: 2rem;
+  }
+</style>
