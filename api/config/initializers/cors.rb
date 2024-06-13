225e5a3a6605c71faa9c8c0ed79e3a16a106e626
Louis Ramos <louisramosdev@gmail.com>
2024-06-13 23:32:32 +0200

    fix(render): move client url to env variable

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins ENV.fetch("CLIENT_URL")
 
     resource "*",
       expose: %w[Authorization],
